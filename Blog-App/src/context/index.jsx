3cee310758ec3b923aa0dcaf4191cf60c3b4201d
Shubham kumar <shubhamkalwar5401@gmail.com>
2024-01-26 16:20:56 +0530

    push

---
+++ Blog-App/src/context/index.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { createContext } from "react";
+
+export const GlobalContext = createContext(null);
+
+export default function GlobalState({ children }) {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
+
+  return (
+    <GlobalContext.Provider value={{ formData, setFormData }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+}
---
+++ Blog-App/src/pages/add-blog/index.jsx
@@ -0,0 +1,54 @@
+import { useContext } from "react";
+import classes from "./styles.module.css";
+import { GlobalContext } from "../../context";
+import axios from 'axios';
+export default function AddNewBlog() {
+  const { formData, setFormData } = useContext(GlobalContext);
+
+  console.log(formData);
+
+  async function handleSaveBlogToDatabase(){
+    const response = await axios.post('http://localhost:5000/api/blogs/add', {
+        title : formData.title,
+        description : formData.description
+    })
+
+    const result = await response.data;
+
+    console.log(result);
+  }
+
+  return (
+    <div className={classes.wrapper}>
+      <h1>Add A Blog</h1>
+      <div className={classes.formWrapper}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Enter Blog Title"
+          id="title"
+          value={formData.title}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              title: e.target.value,
+            })
+          }
+        />
+        <textarea
+          name="description"
+          placeholder="Enter Blog Description"
+          id="description"
+          value={formData.description}
+          onChange={(event) =>
+            setFormData({
+              ...formData,
+              description: event.target.value,
+            })
+          }
+        />
+        <button onClick={handleSaveBlogToDatabase}>Add New Blog</button>
+      </div>
+    </div>
+  );
+}
