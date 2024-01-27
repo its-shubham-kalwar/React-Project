4288293d124f60266f6dacd800c5ad70b7469454
Shubham kumar <shubhamkalwar5401@gmail.com>
2024-01-27 10:06:46 +0530

    push

---
+++ SHOPPING-CART/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import Cart from './pages/cart'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Navbar</h1>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ SHOPPING-CART/src/components/header/index.jsx
@@ -0,0 +1,12 @@
+export default Header(){
+    return (<div>
+        <nav className="flex, items-center justify-between h-20 max-w-6xl mx-auto">
+            <Link to={'/'}>
+            <div className="ml-5">
+            <h1>REACT</h1>
+            </div>
+            </Link>
+        </nav>
+    </div>
+    )
+}
